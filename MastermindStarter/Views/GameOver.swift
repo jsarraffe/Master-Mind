02355a83485718512dde0e63a58d70547a3bb2b7
Jacques Sarraffe <jsarraffe@gmail.com>
2021-03-12 16:58:26 -0800

    RESET TO THIS ONE

---
+++ MastermindStarter/Views/GameOver.swift
@@ -0,0 +1,20 @@
+//
+//  GameOver.swift
+//  MastermindStarter
+//
+//  Created by Jacques Sarraffe on 3/12/21.
+//
+
+import SwiftUI
+
+struct GameOver: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameOver_Previews: PreviewProvider {
+    static var previews: some View {
+        GameOver()
+    }
+}
